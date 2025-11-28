c2ac4742e15ecf7b3343712f5f95375eb544388a
vdzhuteva <v.k.dzhuteva@gmail.com>
2025-11-28 16:14:09 +0200

    Implement Home and Catalog components, and update routing in App component.

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,24 +1,26 @@
+import { Link } from 'react-router';
+
 export default function Header() {
     return (
         <header>
-        {/* Navigation */}
-        <nav>
-          <a className="home" href="#">
-            {" "}
-            <img src="./images/logo.png" alt="logo" />{" "}
-          </a>
-          <a href="#">Catalog</a>
-          {/* Logged-in users */}
-          <div id="user">
-            <a href="#">Add Game</a>
-            <a href="#">Logout</a>
-          </div>
-          {/* Guest users */}
-          <div id="guest">
-            <a href="#">Login</a>
-            <a href="#">Register</a>
-          </div>
-        </nav>
-      </header>
+            {/* Navigation */}
+            <nav>
+                <Link className="home" to="/">
+                    {" "}
+                    <img src="./images/logo.png" alt="logo" />{" "}
+                </Link>
+                <Link to="/games">Catalog</Link>
+                {/* Logged-in users */}
+                <div id="user">
+                    <a href="#">Add Game</a>
+                    <a href="#">Logout</a>
+                </div>
+                {/* Guest users */}
+                <div id="guest">
+                    <a href="#">Login</a>
+                    <a href="#">Register</a>
+                </div>
+            </nav>
+        </header>
     )
 }
